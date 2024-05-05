c68e77d891206422384f27221a876ba8db71b5da
SHIRISH <shirish.shrestha07@gmail.com>
2024-05-05 22:17:43 +0545

    marquee effect in gsap

---
+++ scroll/script.js
@@ -0,0 +1,27 @@
+function marqueeAnimation() {
+  window.addEventListener("wheel", function (dets) {
+    if (dets.deltaY > 0) {
+      gsap.to(".marquee", {
+        transform: "translateX(-200%)",
+        duration: 3,
+        repeat: -1,
+        ease: "none",
+      });
+      gsap.to(".marquee img", {
+        rotate: 180,
+      });
+    } else {
+      gsap.to(".marquee", {
+        transform: "translateX(0%)",
+        duration: 3,
+        repeat: -1,
+        ease: "none",
+      });
+      gsap.to(".marquee img", {
+        rotate: 0,
+      });``
+    }
+  });
+}
+
+marqueeAnimation();
